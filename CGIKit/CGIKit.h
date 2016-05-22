8f0641396c0eb357ae819cc41599afa4813934e3
Maxthon Chan <xcvista@me.com>
2016-05-22 17:54:37 +0800

    Initial Commit

---
+++ CGIKit/CGIKit.h
@@ -0,0 +1,19 @@
+//
+//  CGIKit.h
+//  CGIKit
+//
+//  Created by Maxthon Chan on 5/22/16.
+//  Copyright © 2016 DreamCity. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CGIKit.
+FOUNDATION_EXPORT double CGIKitVersionNumber;
+
+//! Project version string for CGIKit.
+FOUNDATION_EXPORT const unsigned char CGIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGIKit/PublicHeader.h>
+
+
